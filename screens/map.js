6f74004a804f0c6abe0d024af93819a9056283a0
jouni heimonen <jouni.heimonen@hotmail.fi>
2024-02-12 15:01:54 +0200

    pävitys

--- screens/map.js
+++ screens/map.js
@@ -1,43 +1,42 @@
 import { useEffect, useState } from 'react';
 import {StyleSheet} from 'react-native';
-import MapView from 'react-native-maps'
-import * as Location from 'expo-location';
+import MapView, { Marker } from 'react-native-maps'
 
-export default function Map () {
 
-    const getUserPosition = async () => {
-        let {status} = await Location.requestForegroundPermissionsAsync()
 
-        try {
-            if (status !== 'granted') {
-                console.log('Geolocation failed')
-                return
-            }
-            const position = await Location.getCurrentPositionAsync({accuracy: Location.Accuracy.High})
-            setLocation({...location,"latitude": position.coords.latitude,"longitude": position.coords.longitude})
-        }catch (error) {
-            console.log(error)
-        }
-    }
-    useEffect (() => {
-        (async() => {
-            getUserPosition()
-        })()
-    }, [])
-    
-    const [location, setLocation] = useState({
-        latitude: 65.0800,
-        longitude: 25.4800,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421
-    })
+export default function Map (props) {
+    const [marker, setMarker] = useState(null)
+
+    /* useEffect (() => {
+    (async() => {
+        getUserPosition()
+    })()
+}, [])
+    */
+
+    const showMarker = (e) => {
+        const coords = e.nativeEvent.coordinate
+        setMarker(coords)
+      }
+
+   
     
     return (
         <MapView
             style={styles.map}
-            region={location}
-            />
-    )
+            region={props.location}
+            mapType='satellite'
+            onLongPress={showMarker}
+        >
+                {marker &&
+                <Marker
+                title='My marker'
+                coordinate={{latitude: marker.latitude, longitude: marker.longitude}}
+                />
+            
+            }
+        </MapView>        
+    );
 }
 
 const styles = StyleSheet.create ({
